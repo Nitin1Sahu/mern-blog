b2b95cebb0181aaf3c38b1b04ac0cf561cc5d38a
Nitin Sahu <sahunitin970@gmail.com>
2024-04-03 20:37:50 +0530

    create page and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 const App = () => {
   return (
-    <div className='text-3xl font-bold'>App</div>
+    <Outlet />
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { About, Dashboard, Home, Projects, Signin, Signup } from './pages'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='about' element={<About />}></Route>
+      <Route path='sign-in' element={<Signin />}></Route>
+      <Route path='sign-up' element={<Signup />}></Route>
+      <Route path='dashboard' element={<Dashboard />}></Route>
+      <Route path='projects' element={<Projects />}></Route >
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
