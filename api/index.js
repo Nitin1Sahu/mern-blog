9f6e1780ea65c4a6c983ed035538f78688ce9f59
Nitin Sahu <sahunitin970@gmail.com>
2024-04-04 12:50:04 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
-const app = express();
+import mongoose from 'mongoose'
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB is connected'))
+    .catch((err) => console.log(`Error : ${err}`))
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000, () => {
-    console.log(`Server is running -> http://localhost:${3000}`)
+app.listen(PORT, () => {
+    console.log(`Server is running -> http://localhost:${PORT}`)
 })
