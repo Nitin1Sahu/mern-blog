4f2186a5d2593efad9da8cd0f94d080b741ec9ba
Nitin Sahu <sahunitin970@gmail.com>
2024-04-04 13:50:59 +0530

    create the test api route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import userRoutes from './routes/user.route.js'
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB is connected'))
     .catch((err) => console.log(`Error : ${err}`))
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
+app.use('/api/user', userRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running -> http://localhost:${PORT}`)
 })
